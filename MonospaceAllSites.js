ee2139fe6112ba286dc92345c3baa327a8fd95d9
Christian Memije <7193975+christianmemije@users.noreply.github.com>
2023-04-06 23:41:37 -0700

    feat(): Add Monospace All Sites

---
+++ MonospaceAllSites.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Monospace All Sites
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Monospace everything. Keep things simple.
+// @author       You
+// @match        *://*/*
+// @grant        none
+// @run-at document-body
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const style = document.createElement('style');
+  style.innerHTML = `
+*:not([class*="icon"], [class*="symbol"] , i) {
+  font-family: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace !important;
+}
+       `;
+  document.getElementsByTagName('head')[0].appendChild(style);
+})();
