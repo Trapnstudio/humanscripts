bab2447a8dec9962c7ab21a45f1aa27367ccdfb4
Christian Memije <7193975+christianmemije@users.noreply.github.com>
2023-04-06 23:27:43 -0700

    feat(): add Sort Google Domains

---
+++ SortGoogleDomains.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Sort Google Domains
+// @namespace    https://greasyfork.org/en/users/673321-christianmemije
+// @version      0.4
+// @description  Sort the search results from Google Domains (domains.google.com)
+// @author       Memije.io
+// @match https://domains.google.com/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  // @ts-check
+  {
+    const observer = new MutationObserver((mutations, observer) => {
+      const allDomainElems = document.querySelectorAll(
+        'search-result-card-domain-name h2.cdk-visually-hidden',
+      );
+
+      const domains = Array.from(allDomainElems)
+        .map((item) => item.textContent)
+        .filter((item) => !(item || '').includes('unavailable'))
+        .map((item) => (item || '').split(' ')[0]);
+      const sorted = [...domains]
+        .sort((a, b) => a.localeCompare(b))
+        .sort((a, b) => (a || '').length - (b || '').length);
+      if (sorted.length > 0) {
+        console.clear();
+        console.log(JSON.stringify(sorted));
+      }
+    });
+
+    // define what element should be observed by the observer
+    // and what types of mutations trigger the callback
+    observer.observe(document, {
+      subtree: true,
+      attributes: true,
+      //...
+    });
+  }
+})();
