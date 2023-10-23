62526a115bec553bd32be77f1f24eb0c8701a7e4
Christian Memije <7193975+christianmemije@users.noreply.github.com>
2023-10-23 07:20:27 -0700

    feat(): improves qr code

--- QRFromHighlight.js
+++ QRFromHighlight.js
@@ -1,8 +1,9 @@
+// @ts-check
 // ==UserScript==
-// @name         QR from highlight
+// @name         QR from selection / highlight
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Create a QR code from anything highlighted
+// @version      0.2
+// @description  Create a QR code from text selected / highlighted.
 // @match        http://*/*
 // @match        https://*/*
 // @grant        none
@@ -12,18 +13,19 @@
 (function () {
   'use strict';
 
-  // Function to get the currently selected text
-  function getSelectionText() {
-    var selectedText = '';
-    if (window.getSelection) {
-      // All modern browsers and IE9+
-      selectedText = window.getSelection().toString();
-    }
-    return selectedText;
-  }
+  const getSelectedText = () => {
+    const activeEl = document.activeElement;
+    const activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
 
-  // Function to generate and display QR code
-  function generateQRCode(text) {
+    return activeElTagName === 'textarea' || activeElTagName === 'input'
+      ? activeEl?.value.substring(
+          activeEl.selectionStart,
+          activeEl.selectionEnd,
+        )
+      : window?.getSelection()?.toString();
+  };
+
+  const createAndAppendQR = (text) => {
     let qrElement = document.getElementById('generated-qr-code');
     if (!qrElement) {
       qrElement = document.createElement('div');
@@ -31,31 +33,25 @@
       document.body.appendChild(qrElement);
     }
 
-    qrElement.innerHTML = ''; // Clear any previous QR codes
-
-    let typeNumber = 0; // Auto detect QR code type based on input data
-    let errorCorrectionLevel = 'L'; // Low error correction level
-    let qr = qrcode(typeNumber, errorCorrectionLevel);
+    const qr = qrcode(0, 'L');
     qr.addData(text);
     qr.make();
 
-    // Add QR code to the container
     qrElement.innerHTML = qr.createImgTag(5);
+    Object.assign(qrElement.style, {
+      position: 'fixed',
+      bottom: '0',
+      right: '0',
+      zIndex: '999999999',
+    });
+  };
 
-    // Style the container
-    qrElement.style.width = '200px';
-    qrElement.style.height = '200px';
-    qrElement.style.position = 'fixed';
-    qrElement.style.bottom = '10px';
-    qrElement.style.right = '10px';
-    qrElement.style.zIndex = '1000';
-  }
+  const handleEvent = () => {
+    const selectedText = getSelectedText();
+    if (selectedText) createAndAppendQR(selectedText);
+  };
 
-  // Listen for text selection
-  document.addEventListener('mouseup', function () {
-    let selectedText = getSelectionText();
-    if (selectedText) {
-      generateQRCode(selectedText);
-    }
+  ['mouseup', 'keyup'].forEach((eventType) => {
+    document.addEventListener(eventType, handleEvent);
   });
 })();
